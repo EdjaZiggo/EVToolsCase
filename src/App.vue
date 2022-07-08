48fa3bd1d15592b72af2c04ed36ef9df7fcbd442
Edja <edja@ziggo.nl>
2022-07-08 14:44:40 +0200

    Initial setup Mapbox

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,97 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">Sarma</router-link>
-  </nav> -->
-  <router-view />
+  <button v-for="(bird, index) in Array.from(birds)" :key="index" @click="birdLayers">{{ bird }}</button>
+  <div id="map" />
+  <!-- <router-view /> -->
 </template>
+<script>
+import mapboxgl from "mapbox-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { onMounted, inject, ref } from 'vue';
 
+export default {
+  setup() {
+    const axios = inject('axios');
+    const url = 'https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=VOGELS&THEMA=vogels';
+    const birds = ref(new Set());
+    
+    
+    function birdLayers(event) {
+      console.log(event)
+    }
+
+    onMounted(async () => {
+      const api = await axios.get(url);
+      api.data.features.map(f => !/overig/i.test(f.properties.Vogel) && birds.value.add(f.properties.Vogel));
+
+      mapboxgl.accessToken = 'pk.eyJ1IjoiZWRqYSIsImEiOiJjbDR6aGd4cDgwa2JiM3B1bDZ3NmgzaGpsIn0.oET3mVlFz2tbZDeJVBIrFg';
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [4.901157, 52.370217],
+        zoom: 13
+      });
+
+      map.on('load', () => {
+        map.addSource('birds', {
+          type: 'geojson',
+          // Use a URL for the value for the `data` property.
+          data: api.data
+        });
+      })
+       
+      map.on('data', async () => {
+        // TODO: Here we want to load a layer
+        // TODO: Here we want to load/setup the popup\
+        
+        // const data = api.data
+        // data.features = data.features.filter(f => f.properties.Vogel === value.value);
+        // console.log(data)
+
+        
+        if (map.getSource('birds') && !map.getLayer('birds-layer')) {
+          map.addLayer({
+            'id': 'birds-layer',
+            'type': 'circle',
+            'source': 'birds',
+            'paint': {
+              'circle-radius': 4,
+              'circle-color': [
+                'match',
+                ['get', 'Vogel'],
+                'Spreeuw',
+                'green',
+                'Huismus',
+                'orange',
+                'Gierzwaluw',
+                'blue',
+                'Boerenzwaluw',
+                'red',
+                'Huiszwaluw',
+                'black',
+                'Overig',
+                'grey',
+                'overig',
+                'grey',
+                'white'
+              ],
+              'circle-stroke-width': 1,
+              //'circle-color': 'red',
+              'circle-stroke-color': 'black'
+            }
+          });
+        }
+      });
+    });
+
+    return {
+      birds,
+      birdLayers
+    }
+  }
+}
+</script>
 <style lang="scss">
+body { margin: 0; padding: 0; }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +110,8 @@ nav {
       color: #42b983;
     }
   }
+}
+#map {
+  height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueAxios, axios)
+  app.provide('axios', app.config.globalProperties.axios)
+  app.mount('#app')
